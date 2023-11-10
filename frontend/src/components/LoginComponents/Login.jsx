694e379ecc1edd7a6c1a79bb917f7a41c824d733
Evan <evan.lueber@stud.kbw.ch>
2023-11-10 14:52:56 +0100

    fetching implemented for login

--- frontend/src/components/LoginComponents/Login.jsx
+++ frontend/src/components/LoginComponents/Login.jsx
@@ -3,6 +3,55 @@ import { BiArrowBack } from "react-icons/bi";
 import Button from "./Button";
 
 const Login = ({ title, setLandingPage, setShowLogin, setNewUser, setChatApp, newUser }) => {
+  const handleLogin = async (username, password) => {
+    try {
+      const response = await fetch('https://localhost:5000/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+      });
+  
+      const data = await response.json();
+  
+      if (response.ok) {
+        console.log('Login successful');
+        console.log('UserId:', data.userId);
+      } else {
+        console.error('Login failed:', data.message);
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleRegistration = async () => {
+    try {
+      const response = await fetch('https://localhost:5000/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        console.log('Registration successful');
+      } else {
+        console.error('Registration failed:', data.message);
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+  
+
   return (
     <div className="login-page">
       <p className="chat-app-title">{title}</p>
