48d6b2f5859b779e3baa954952511062274069ea
Evan <evan.lueber@stud.kbw.ch>
2023-11-23 13:34:48 +0100

    buttons at the left done

--- frontend/src/components/ChatComponents/ChatApp.jsx
+++ frontend/src/components/ChatComponents/ChatApp.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
-import RightSide from './RightSide/MainComponent'
-import LeftSide from './LeftSide/MainComponent'
+import React from "react";
+import RightSide from "./RightSide/MainComponent";
+import LeftSide from "./LeftSide/MainComponent";
 
 const ChatApp = ({ username }) => {
+  const [showStandard, setShowStandard] = React.useState(true);
+  const [showConnect, setShowConnect] = React.useState(false);
+  const [showChannels, setShowChannels] = React.useState(false);
+
   return (
-    <div className='flex flex-row w-screen h-screen'>
-      <LeftSide username={username}/>
+    <div className="flex flex-row w-screen h-screen">
+      <LeftSide
+        username={username}
+        setShowChannels={setShowChannels}
+        setShowConnect={setShowConnect}
+        setShowStandard={setShowStandard}
+      />
       <RightSide />
     </div>
-  )
-}
+  );
+};
 
-export default ChatApp
+export default ChatApp;
--- frontend/src/components/ChatComponents/LeftSide/MainComponent.jsx
+++ frontend/src/components/ChatComponents/LeftSide/MainComponent.jsx
@@ -1,14 +1,47 @@
-import React from 'react'
+import React from "react";
+import { IoIosArrowForward } from "react-icons/io";
+import { BiLogOut } from "react-icons/bi";
 
-const MainComponent = ({username}) => {
-
+const MainComponent = ({
+  username,
+  setShowConnect,
+  setShowChannels,
+  setShowStandard,
+}) => {
   return (
-    <div className='flex flex-col w-1/4 h-screen text-white border-r-2 border-r-white'>
-      <div className='h-full border-r-2 border-r-white'>
-        <p>{username}</p>
+    <div className="flex flex-col w-1/4 h-screen ">
+      <div className="text-center p-4 border-b border-b-white border-r border-r-white text-white">
+        <p className="text-4xl">{username}</p>
+      </div>
+      <div className="flex flex-col text-xl p-5 items-start gap-5 border-r border-r-white text-white h-screen w-1/2 ">
+        <button
+          className="flex flex-row"
+          onClick={() => {
+            setShowChannels(true);
+            setShowConnect(false);
+            setShowStandard(false);
+          }}
+        >
+          Channels <IoIosArrowForward className="mt-1.5 ml-1" />
+        </button>
+        <button
+          className="flex flex-row"
+          onClick={() => {
+            setShowChannels(false);
+            setShowConnect(true);
+            setShowStandard(false);
+          }}
+        >
+          Connect <IoIosArrowForward className="mt-1.5 ml-1" />
+        </button>
+      </div>
+      <div className="flex flex-col text-xl p-5 items-start gap-5 border-r border-r-white text-white w-1/2 ">
+        <button className="flex flex-row">
+          <BiLogOut className="mt-1 mr-1" /> Logout
+        </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default MainComponent
+export default MainComponent;
