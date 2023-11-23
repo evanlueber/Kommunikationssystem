4912ba5d8f1171dcfa9bd3fbf2d8634e45d19116
evanlueber <evan.lueber@lernende.bbw.ch>
2023-11-23 20:25:47 +0100

    display joinid

--- frontend/src/components/ChatComponents/RightSide/MainComponent.jsx
+++ frontend/src/components/ChatComponents/RightSide/MainComponent.jsx
@@ -7,7 +7,7 @@ const MainComponent = () => {
   return (
     <div className="flex flex-col w-3/4 ">
       <div className="text-center p-4 border-b border-b-white text-white">
-        <p className="text-4xl">{showConnect ? "Connect": currentChannel.channelName ? currentChannel.channelName: "Channel"}</p>
+        <p className="text-4xl">{showConnect ? "Connect": currentChannel.channelName ? currentChannel.channelName + "\t#" + currentChannel.channelJoinId: "Channel"}</p>
       </div>
         <div className='flex flex-col justify-center items-center h-full'>
           {showConnect&& <Connect/>}
