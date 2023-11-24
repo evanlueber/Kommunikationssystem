a017dc8b5a1070c88ed481452ba961ba38d09a17
Evan <evan.lueber@stud.kbw.ch>
2023-11-24 14:40:43 +0100

    message design edited

--- frontend/src/components/ChatComponents/RightSide/Message.jsx
+++ frontend/src/components/ChatComponents/RightSide/Message.jsx
@@ -8,10 +8,10 @@ const Message = ({message}) => {
   const sentAt = new Date(message.sentAt)
   return (
     <div className={'w-full flex flex-col mb-6 ' + (side === "right" ? "items-end": "items-start" )}>
-      <div className={" flex flex-col justify-between rounded p-2 " + (side === "right" ? " bg-green-500 ": "bg-purple-600" )}>
+      <div className={" flex flex-col justify-between rounded-xl p-2 min-w-[15rem] " + (side === "right" ? " bg-green-500 ": "bg-purple-600" )}>
         <p className="text-2xl font-bold">{message.username}</p>
         <p className="text-xl">{message.content}</p>
-        <p className="text-sm opacity-50 ">{sentAt.getHours() < 10 ? "0"+sentAt.getHours(): sentAt.getHours()}:{sentAt.getMinutes() < 10 ? "0"+sentAt.getMinutes(): sentAt.getMinutes()}</p>
+        <p className="text-sm opacity-50 text-right ">{sentAt.getHours() < 10 ? "0"+sentAt.getHours(): sentAt.getHours()}:{sentAt.getMinutes() < 10 ? "0"+sentAt.getMinutes(): sentAt.getMinutes()}</p>
       </div>
     </div>
   )
